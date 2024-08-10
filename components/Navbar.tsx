328d4fbffd51af707aae37a879bfc424651d4313
erandi2002 <emunasinghe75@gmail.com>
2024-08-10 14:18:49 +0530

    Update Navbar links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -6,8 +7,17 @@ const Navbar = () => {
     <nav className="flexBetween max-container
     padding-container relative z-30 py-5">
       <Link href="/">
-        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} /> 
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
+
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter
+            cursor-pointer pb-1.5 transition-all hover:font-bold">
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
   )
 }
