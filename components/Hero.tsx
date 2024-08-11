3f7dbc97ddd0cdd42837597bf018cccc0bd483a2
erandi2002 <emunasinghe75@gmail.com>
2024-08-11 10:57:05 +0530

    Complete Hero section layout and styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,11 +5,9 @@ const Hero = () => {
   return (
     <section
       className="max-container padding-container flex flex-col gap-20 py-10
-       pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500 "
+       pb-32 md:gap-28 lg:py-20 xl:flex-row"
     >
       <div className="hero-map" />
-
-      {/* LEFT */}
 
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <Image
@@ -49,15 +47,38 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-col w-full gap-3 sm:flex-row ">
-          <Button 
-             type="button" 
-             title="Download App" 
-             variant="btn_green" />
-          <Button 
-             type="button" 
-             title="How we work?" 
-             icon="/play.svg"
-             variant="btn_white_text" />   
+          <Button type="button" title="Download App" variant="btn_green" />
+          <Button
+            type="button"
+            title="How we work?"
+            icon="/play.svg"
+            variant="btn_white_text"
+          />
+        </div>
+      </div>
+
+      <div className="relative flex flex-1 items-start">
+        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8 ">
+          <div className="flex flex-col ">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-20">Location</p>
+              <Image src="/close.svg" alt="close" width={24} height={24} />
+            </div>
+            <p className="bold-20 text-white ">Adam's Peak</p>
+          </div>
+
+          <div className="flexBetween">
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20 ">Distance</p>
+              <p className="bold-20 text-white ">80.45 km</p>
+            </div>
+
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20 ">Elevation</p>
+              <p className="bold-20 text-white ">2,243 m</p>
+            </div>
+
+          </div>
         </div>
       </div>
     </section>
