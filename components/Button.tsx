894139f570d6a8b41643baeefa47ec29a7f10299
erandi2002 <emunasinghe75@gmail.com>
2024-08-12 15:29:02 +0530

    Add GetApp component with buttons and image

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,16 +5,19 @@ type ButtonProps = {
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 };
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      }`}
       type={type}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
   );
 };
