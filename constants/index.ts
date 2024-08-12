7d25418631930394014f003cf6767c0904767d32
erandi2002 <emunasinghe75@gmail.com>
2024-08-12 21:27:01 +0530

    Finish footer section and mark the website as complete

--- constants/index.ts
+++ constants/index.ts
@@ -62,15 +62,15 @@ export const FOOTER_LINKS = [
     },
     {
         title: 'Our Community',
-        links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+        links: ['Climbing xixixi', 'Hiking hilink', 'Hilink Trail'],
     },
 ];
 
 export const FOOTER_CONTACT_INFO = {
     title: 'Contact Us',
     links: [
-        { label: 'Admin Officer', value: '123-456-7890' },
-        { label: 'Email Officer', value: 'hilink@akinthil.com' },
+        { label: 'Admin Officer', value: '+94-11-234-5678' },
+        { label: 'Email Officer', value: 'info@hilink.lk' },
     ],
 };
 
